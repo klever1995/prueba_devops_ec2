c9104cc47d534640f1407d776ed3b42891f23c67
klever1995 <klever_mix@hotmail.com>
2024-12-04 10:47:15 -0600

    feat: :sparkles: nueva version

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Despliegue en Instancia EC2</title>
+    <title>Document</title>
+    <h1>hola mundo</h1>
+
+
+    <h1>hello</h1>
+    <h1>klever robalino</h1>
+    <h1>ultimo cambio</h1>
+    <h1>raquel lopez<h1>
+    <h1>hola mundo</h1>
+    <h1>hola mundo</h1>
+
 </head>
 <body>
-    <h1>Bienvenido al sistema de despliegue en EC2</h1>
-    <p>Esta página se está mostrando desde la instancia EC2 con la siguiente dirección IP:</p>
-
-    <?php
-    // Mostrar la dirección IP del servidor (instancia EC2)
-    echo '<p><strong>Dirección IP de la instancia EC2:</strong> ' . $_SERVER['SERVER_ADDR'] . '</p>';
-
-    // Mostrar información adicional (puedes personalizar esto para mostrar más detalles)
-    echo '<p><strong>Nombre del host:</strong> ' . gethostname() . '</p>';
-    ?>
-
-    <p>La aplicación se ha desplegado correctamente en esta instancia EC2.</p>
+    <?php echo gethostname();?>
 </body>
 </html>
